076bfbe5d90fcb528f66904a6aea4406f5c2a246
047c <soroksedmoyskorostnoy@gmail.com>
2024-09-26 18:24:09 +0300

    Fix bugs

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 
 x = int(input("Введите число x: "))
 y = int(input("Введите число y: "))
-psi = (math.fabs((x ** y / x) - ((y / x) ** (1. / 3.)))) + (y - x)
+psi = (math.fabs((x**(y/x)) - ((y/x)** (1. / 3.))) + (y-x))
 print(f"Значение функции: {psi}")
