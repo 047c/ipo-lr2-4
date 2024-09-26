44194d0f76feea22f27e5cf09c0caf73f54b2b6a
047c <soroksedmoyskorostnoy@gmail.com>
2024-09-26 18:16:32 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import math
+
+x = int(input("Введите число x: "))
+y = int(input("Введите число y: "))
+psi = (math.fabs((x ** y / x) - ((y / x) ** (1. / 3.)))) + (y - x)
+print(f"Значение функции: {psi}")
